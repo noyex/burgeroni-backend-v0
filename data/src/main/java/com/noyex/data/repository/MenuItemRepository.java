89beeb7b538935d6a398dbc88f5b650de595235b
noyex <153921909+noyex@users.noreply.github.com>
2025-05-27 08:59:49 +0200

    new email css

--- data/src/main/java/com/noyex/data/repository/MenuItemRepository.java
+++ data/src/main/java/com/noyex/data/repository/MenuItemRepository.java
@@ -1,6 +1,7 @@
 package com.noyex.data.repository;
 
 import com.noyex.data.model.MenuItem;
+import com.noyex.data.model.order.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
